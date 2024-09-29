314e456a8c36b7b5c1c9418398ec45fd40bda7dc
Simba_The_King_Lion <thekingsimbadev@gmail.com>
2024-09-29 13:55:27 +0100

    basic project config

---
+++ react-frontend/vite.config.js
@@ -0,0 +1,37 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig(({ command }) => {
+  const isProduction = command === "build";
+  return {
+    base: "/ennov-test/",
+    mode: isProduction ? "production" : "development",
+    loader: { ".js": "jsx" },
+    plugins: [react()],
+    server: {
+      host: "localhost",
+      open: true,
+      port: 3100,
+      headers: {
+        "Strict-Transport-Security": "max-age=86400; includeSubDomains",
+        "X-Content-Type-Options": "nosniff",
+        "X-Frame-Options": "DENY",
+        "X-XSS-Protection": "1; mode=block",
+        "Content-Security-Policy": "upgrade-insecure-requests",
+      },
+    },
+    build: {
+      outDir: "dist",
+      sourcemap: true,
+      commonjsOptions: {
+        include: [/node_modules/],
+      },
+    },
+    resolve: {
+      alias: {
+        "@": "/src",
+        src: "/src",
+      },
+    },
+  };
+});
